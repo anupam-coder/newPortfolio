0a4197d0edd6d3435aad7c8edce3f9dd599ffb72
anupam-coder <anupampatrabbsr1995@gmail.com>
2024-04-27 12:48:58 +0530

    fixed experience section for desktop

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -1,5 +1,5 @@
 import React from "react";
-import experienceStyles from "./experience.styles";
+import "./experience.css"; // Import CSS file with your defined classes
 import { ReactComponent as CompanyIcon } from "../../assets/company/company.svg";
 import FractalLogo from "../../assets/company/wipro-logo.png";
 import accentureLogo from "../../assets/company/accenture-logo.png";
@@ -7,69 +7,58 @@ import opentextLogo from "../../assets/company/ot_logo.png";
 import Header from "../Header/Header";
 
 const Experience = () => {
-  const classes = experienceStyles();
   return (
-    <section
-      id="experience"
-      className={`${classes.experienceContainer} container`}
-    >
+    <section id="experience" className="experience-container container">
       <Header heading="Professional Experience" details="" />
-      <div className={classes.expBlock}>
-        <div className={classes.expBlockRight}></div>
-        <span
-          className={`${classes.expBlockSeperator} ${classes.expBlockSeperatorFirst}`}
-        >
-          <span className={classes.presentDayTag}>Present Day</span>
-          <span className={`hide-md ${classes.expYearTagLeft}`}>June 2023</span>
-          {/* <span className={`hide-sm ${classes.expYearTagRight}`}>Feb 2019</span> */}
+      <div className="exp-block">
+        <div className="exp-block-right"></div>
+        <span className="exp-block-separator exp-block-separator-first">
+          <span className="present-day-tag">Present Day</span>
+          <span className="hide-md exp-year-tag-left">June 2023</span>
+          {/* <span className="hide-sm exp-year-tag-right">Feb 2019</span> */}
         </span>
-        <div className={classes.expBlockRight}>
+        <div className="exp-block-right">
           <div className="exp-block-content">
-            <div className={classes.companyLogo}>
+            <div className="company-logo">
               <img src={opentextLogo} alt="OpenText-logo"></img>
             </div>
-            <div className={classes.companyName}>OpenText</div>
-            <div className={classes.role}>Senior Software Engineer</div>
+            <div className="company-name">OpenText</div>
+            <div className="role">Senior Software Engineer</div>
           </div>
         </div>
       </div>
-      <div className={classes.expBlock}>
-        <div className={classes.expBlockRight}>
-          <div className="exp-block-content hide-md">
-            <div className={classes.companyLogo}>
+      <div className="exp-block">
+        <div className="exp-block-right">
+          <div className="exp-block-content block-left hide-md">
+            <div className="company-logo">
               <img src={accentureLogo} alt="Accenture-logo"></img>
             </div>
-            <div className={classes.companyName}>Accenture</div>
-            <div className={classes.role}>
-              Application Development Senior Analyst
-            </div>
+            <div className="company-name">Accenture</div>
+            <div className="role">Application Development Senior Analyst</div>
           </div>
         </div>
-        <span className={classes.expBlockSeperator}>
-          <span className={classes.expYearTagRight}>Sept 2021</span>
+        <span className="exp-block-separator">
+          <span className="exp-year-tag-right">Sept 2021</span>
         </span>
-        <div className={classes.expBlockRight}>
+        <div className="exp-block-right">
           <div className="exp-block-content hide-sm">
-            <div className={classes.companyLogo}></div>
+            <div className="company-logo"></div>
           </div>
         </div>
       </div>
-      <div className={classes.expBlock}>
-        <div className={classes.expBlockRight}>
+      <div className="exp-block">
+        <div className="exp-block-right"></div>
+        <span className="exp-block-separator exp-block-separator-first">
+          <span className="hide-md exp-year-tag-left">Nov 2017</span>
+        </span>
+        <div className="exp-block-right">
           <div className="exp-block-content hide-md">
-            <div className={classes.companyLogo}>
+            <div className="company-logo">
               <img src={FractalLogo} alt="Fractal-Logo"></img>
             </div>
-            <div className={classes.companyName}>Wipro</div>
-            <div className={classes.role}>Associate Consultant</div>
-          </div>
-        </div>
-        <span className={classes.expBlockSeperator}>
-          <span className={classes.expYearTagRight}>Nov 2017</span>
-        </span>
-        <div className={classes.expBlockRight}>
-          <div className="exp-block-content hide-sm">
-            <div className={classes.companyLogo}></div>
+            <div className="company-name">Wipro</div>
+            <div className="role">Associate Consultant</div>
+            <div className="company-logo"></div>
           </div>
         </div>
       </div>
